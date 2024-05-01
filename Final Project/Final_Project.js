750dba0b5fb14378196448a14a9c666f5d8e8a8a
CameronWillcox <157517439+CameronWillcox@users.noreply.github.com>
2024-05-01 17:46:00 -0500

    Update Final_Project.js

--- Final Project/Final_Project.js
+++ Final Project/Final_Project.js
@@ -1,20 +1,24 @@
 var mymap = L.map("map").setView([44.6085123, -89.6667297], 8);
 
- var grayscale = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
-   attribution: 'Map tiles by Carto, under CC BY 3.0. Data by OpenStreetMap, under ODbL.',
-   maxZoom: 11,
-   minZoom: 2
- }).addTo(mymap);
+var grayscale = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ2NoYXVkaHVyaSIsImEiOiJjazBtcG5odG8wMDltM2JtcjdnYTgyanBnIn0.qwqjMomdrBMG36GQKXBlMw', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox/light-v9',
+    tileSize: 512,
+    zoomOffset: -1
+}).addTo(mymap);
+
+var satellite = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ2NoYXVkaHVyaSIsImEiOiJjazBtcG5odG8wMDltM2JtcjdnYTgyanBnIn0.qwqjMomdrBMG36GQKXBlMw', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' + 'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox/satellite-v9',
+    tileSize: 512,
+    zoomOffset: -1
+}).addTo(mymap);
 
 
 
 
-var myIcon1 = L.icon({
-    iconUrl: 'home.png',
-    iconSize: [25, 35],
-    iconAnchor: [0, 0],
-    popupAnchor: [15,10],
-});
 
 
 var myIcon2 = L.icon({
@@ -47,13 +51,13 @@ var myIcon5 = L.icon({
 
 
 
-dane = L.marker([43.04319, -89.29244], {icon: myIcon2}).bindPopup("<b>Dane County Humane Society <br/><br/> About: Dane County Humane Society's Wildlife Center provides specialized care and treatment to thousands of injured, ill, or orphaned wild animals every year with the goal of releasing healthy animals back to their natural habitats.<br/> <br/> Phone #: (608) 287-3235 <br/> <br/> Address: 5132 Voges Road Madison, WI 53718<br/>").addTo(mymap)
+var dane = L.marker([43.04319, -89.29244], {icon: myIcon2}).bindPopup("<b>Dane County Humane Society <br/><br/> About: Dane County Humane Society's Wildlife Center provides specialized care and treatment to thousands of injured, ill, or orphaned wild animals every year with the goal of releasing healthy animals back to their natural habitats.<br/> <br/> Phone #: (608) 287-3235 <br/> <br/> Address: 5132 Voges Road Madison, WI 53718<br/>").addTo(mymap)
 
-wild = L.marker([45.69043, -89.49236], {icon: myIcon3}).bindPopup("<b>Wild Instincts<br/><br/> About: Providing quality, ethical wildlife rehabilitation while promoting wildlife stewardship through education and research.<br/> <br/> Phone #: 715-362-9453 <br/> <br/> Address: 4621 Apperson Dr Rhinelander, WI 54501<br/>").addTo(mymap)
+var wild = L.marker([45.69043, -89.49236], {icon: myIcon3}).bindPopup("<b>Wild Instincts<br/><br/> About: Providing quality, ethical wildlife rehabilitation while promoting wildlife stewardship through education and research.<br/> <br/> Phone #: 715-362-9453 <br/> <br/> Address: 4621 Apperson Dr Rhinelander, WI 54501<br/>").addTo(mymap)
 
-raptorcenter = L.marker([44.98343, -93.18151], {icon: myIcon4}).bindPopup("<b>The Raptor Center<br/><br/> About: Our mission is to ensure the health of raptors and the world we share. We specialize in the medical care, rehabilitation, and conservation of eagles, hawks, owls, falcons, osprey, and vultures.<br/> <br/> Phone #: 612-624-4745 <br/> <br/> Address: 1920 Fitch Avenue St.Paul, MN 55108<br/>").addTo(mymap)
+var raptorcenter = L.marker([44.98343, -93.18151], {icon: myIcon4}).bindPopup("<b>The Raptor Center<br/><br/> About: Our mission is to ensure the health of raptors and the world we share. We specialize in the medical care, rehabilitation, and conservation of eagles, hawks, owls, falcons, osprey, and vultures.<br/> <br/> Phone #: 612-624-4745 <br/> <br/> Address: 1920 Fitch Avenue St.Paul, MN 55108<br/>").addTo(mymap)
 
-raptoreducation = L.marker([45.10500, -89.10500], {icon: myIcon5}).bindPopup("<b>Raptor Education Group Inc<br/> <br/>About: Dedicated to the care and rehabilitation of injured or orphaned native bird species and public education of wildlife issues.<br/> <br/> Phone #: (715)-623-4015 <br/> <br/> Address: N2160 W Rollwood Rd Antigo, WI 54409<br/>").addTo(mymap)
+var raptoreducation = L.marker([45.10500, -89.10500], {icon: myIcon5}).bindPopup("<b>Raptor Education Group Inc<br/> <br/>About: Dedicated to the care and rehabilitation of injured or orphaned native bird species and public education of wildlife issues.<br/> <br/> Phone #: (715)-623-4015 <br/> <br/> Address: N2160 W Rollwood Rd Antigo, WI 54409<br/>").addTo(mymap)
 
 
 
@@ -121,10 +125,10 @@ var myIcon = new L.Icon({
      iconSize: [20, 20],
      iconAnchor: [10, 15],
      popupAnchor:  [1, -24],
-     iconUrl: './images/Dot.png'
+     iconUrl: 'Dot.png'
      });
     
-var peaks = new L.geoJson(mtn_peaks, {
+var point = new L.geoJson(bird_point, {
     onEachFeature: function(feature, featureLayer){
         featureLayer.bindPopup('<p>Location: <b>'+feature.properties.TITLE+ '</b></br>' +
                                '<p>Injury Type: <b>'+feature.properties.TITLE_2+ '</b>' +
@@ -144,7 +148,7 @@ function getRadius(area) {
   return radius * 15;
 }
 
-var propcircles = new L.geoJson(mtn_peaks, {
+var propcircles = new L.geoJson(bird_point, {
     onEachFeature: function(feature, featureLayer){
         featureLayer.bindPopup('<p>Location: <b>'+feature.properties.TITLE+ '</b></br>' +
                                    'Number of Eagles: '+feature.properties.number_of1+'</p>');
@@ -173,20 +177,36 @@ var propcircles = new L.geoJson(mtn_peaks, {
 
 
 ////Legend/////
+var baseLayers = {
+    'satellite': satellite,
+    'Grayscale': grayscale,
+	};
+
 var overlayMaps = {
     
-    "<img src='./images/Dot.png' height=16> Location of Injured Bald Eagles": peaks,
+    "<img src='Dot.png' height=16> Location of Injured Bald Eagles": point,
     "<img src='3.png' height=16> Number of Eagles in Area": propcircles,
  
 };
 
-var legend = L.control.layers(overlayMaps, {}, {collapsed: false}).addTo(mymap);
 
 
+var legend = L.control.layers(baseLayers, overlayMaps, {collapsed: false}).addTo(mymap);
 
+
+var miniMap = new L.Control.MiniMap(L.tileLayer('https://api.maptiler.com/maps/topo/{z}/{x}/{y}.png?key=tZnptaeI9RvKHsX18rbW'), {
+            toggleDisplay: true,
+            position: 'bottomleft'
+        }).addTo(mymap);
 
 
 L.control.scale({position: 'bottomleft', maxWidth: '150', metric: 'True'}).addTo(mymap);
+
+
+L.easyButton(('<img src="globe_icon.png", height=85%>'), function(btn, map){
+    map.setView([44.6085123, -89.6667297], 8);
+}).addTo(mymap);
+
 
 
 
